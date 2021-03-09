673d915e2b573905225e847444c57ff945628c8d
Jacob Lund Mogensen <JALMO@ADM.RKSK.DK>
2021-03-09 14:46:22 +0100

    added cli options

--- src/bins/toaster/main.rs
+++ src/bins/toaster/main.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[path = "../../wstring.rs"]
-mod wstring;
 #[path = "../../com_helper.rs"]
 mod com_helper;
+#[path = "../../wstring.rs"]
+mod wstring;
 
+use com_helper::InitCom;
 use windows::Interface;
 use windows::{
     data::xml::dom::XmlDocument,
@@ -12,30 +13,34 @@ use windows::{
 };
 use winrt::windows;
 use wstring::WideString;
-use com_helper::InitCom;
 
 use std::{env, path::Path};
 const LAME_SUN: &[u8] = include_bytes!("../../../assets/lame_sun.png");
 
-fn start() -> windows::Result<()> {
+fn start(input: &clap::ArgMatches) -> windows::Result<()> {
     let _com = InitCom::sta()?;
-    let current_file = get_exe_path();
+    let mut current_file = get_exe_path();
+
     let mut shortcut = env::var("APPDATA").unwrap();
     shortcut.push_str(r"\Microsoft\Windows\Start Menu\Programs\Toaster.lnk");
 
-    let mut image_path = env::var("TEMP").unwrap();
-    image_path.push_str("toast_default.png");
+    if let Some(app_id) = input.value_of("AppID") {
+        current_file = app_id.to_string();
+    } else {
+        if let None = input.value_of("NoShortcut") {
+            create_shortcut(&current_file, &shortcut)?;
+        }
+    }
 
-    if create_shortcut(&current_file, &shortcut)? && create_reg_keys(&current_file) {
-        let notification = construct_notification(Some(&image_path))?;
-        
+    if create_reg_keys(&current_file) {
+        let notification = construct_notification(input.value_of("IconPath"))?;
+
         ToastNotificationManager::history()?.clear_with_id(current_file.clone())?;
 
         ToastNotificationManager::get_default()?
             .create_toast_notifier_with_id(current_file)?
             .show(notification)?;
         std::thread::sleep(std::time::Duration::from_millis(10));
-
     } else {
         eprintln!("Failed to create registry keys for: {}", &current_file);
     }
@@ -90,19 +95,34 @@ fn create_reg_keys(file: &str) -> bool {
     result
 }
 
-fn construct_notification(image_path: Option<&str>) -> windows::Result<ToastNotification> {
+fn construct_notification(input_image: Option<&str>) -> windows::Result<ToastNotification> {
     use std::fs;
-    let mut image_tag = String::new();
+    let image_tag;
+
     
-    if let Some(image_path) = image_path {
-        fs::write(image_path, LAME_SUN).unwrap_or_else(|_err| {
-            eprintln!("Failed to save template image as {}", image_path);
+
+    if let Some(image_path) = input_image {
+        if Path::new(image_path).exists() {
+            image_tag = format!(
+                "<image placement=\"appLogoOverride\" hint-crop=\"none\" src=\"{}\"/>",
+                image_path
+            );
+        } else {
+            image_tag = String::new();
+        }
+    } else {
+        let mut defualt_image_path = env::var("TEMP").unwrap();
+        defualt_image_path.push_str("toast_default.png");
+
+        fs::write(&defualt_image_path, LAME_SUN).unwrap_or_else(|_err| {
+            eprintln!("Failed to save template image as {}", &defualt_image_path);
         });
 
-        if Path::new(image_path).exists() {
-            image_tag = format!("<image placement=\"appLogoOverride\" hint-crop=\"none\" src=\"{}\"/>", image_path);
-        }
-    }    
+        image_tag = format!(
+            "<image placement=\"appLogoOverride\" hint-crop=\"none\" src=\"{}\"/>",
+            defualt_image_path
+        );
+    }
 
     let args: Vec<String> = env::args().collect();
     let text01 = match args.get(1) {
@@ -111,12 +131,12 @@ fn construct_notification(image_path: Option<&str>) -> windows::Result<ToastNoti
     };
     let text02 = match args.get(2) {
         Some(s) => s.as_str(),
-        None => ""
+        None => "",
     };
 
     let notification = {
         let xml = XmlDocument::new()?;
-println!("{:?}", LAME_SUN);
+
         let xml_text = format!(
             r#"
 <toast scenario="reminder" launch="developer-pre-defined-string">
@@ -124,14 +144,14 @@ println!("{:?}", LAME_SUN);
     <binding template="ToastGeneric">
       {}
       <text>{}</text>
-      <text>{}</text>
-      
+      <text>{}</text>      
       <text placement="attribution">{}</text>
     </binding>
   </visual>
   <audio src= "ms-winsoundevent:Notification.Default"/>
-</toast>"#, 
-        image_tag, text01, text02, "");
+</toast>"#,
+            image_tag, text01, text02, ""
+        );
 
         xml.load_xml(xml_text)?;
 
@@ -143,8 +163,21 @@ println!("{:?}", LAME_SUN);
     Ok(notification)
 }
 
+fn get_cli_inputs() -> clap::ArgMatches<'static> {
+    use clap::clap_app;
+    clap_app!(Toaster =>
+        (version: "1.0")
+        (about: "An easy way to display Toast notifications\n\nNOTE: It can fail the first time, if the start menu is not updated")
+        (@arg Headline: -h --headline +takes_value "Sets the headline of the Toast")
+        (@arg Text: -t --text +takes_value "Sets the message of the Toast")
+        (@arg IconPath: -i --icon +takes_value "Sets the icon of the Toast")
+        (@arg File: -f --file +takes_value "Uses an XML file as the Toast")
+        (@arg NoShortcut: -n --noshortcut "Toaster doesn't try to create it's a shortcut in start menu (userspace)")
+        (@arg AppID: -a --appid +takes_value "The location the program you want to display the message. The program must have a shortcut in the start menu")
+    ).get_matches()
+}
+
 fn create_shortcut(file: &str, shortcut: &str) -> windows::Result<bool> {
-    use std::ptr::null;
     use windows::win32::com::IPersistFile;
     use windows::win32::shell::{IShellLinkW, ShellLink};
 
@@ -156,17 +189,13 @@ fn create_shortcut(file: &str, shortcut: &str) -> windows::Result<bool> {
 
         unsafe {
             let ok1 = shell_link.SetPath(file_path.ptr()).is_ok();
-            // let ok2 = shell_link.SetDescription(null()).is_ok();
-            let ok3 = shell_link.SetWorkingDirectory(folder.ptr()).is_ok();
-            // let ok4 = shell_link.SetIconLocation(psz_icon_path, i_icon)
+            let ok2 = shell_link.SetWorkingDirectory(folder.ptr()).is_ok();
+            // let ok3 = shell_link.SetIconLocation(psz_icon_path, i_icon)
 
-            if ok1 {
+            if ok1 && ok2 {
+                // shell_link.cast == IShellLink::QueryInterface
                 if let Ok(persist_file) = shell_link.cast::<IPersistFile>() {
-
-                    let mut shortcut_name = WideString::from_str_with_size(
-                        &shortcut,
-                        256,
-                    );
+                    let mut shortcut_name = WideString::from_str_with_size(&shortcut, 256);
 
                     result = persist_file
                         .Save(shortcut_name.mut_ptr(), true.into())
@@ -194,7 +223,8 @@ fn get_exe_path() -> String {
 }
 
 fn main() {
-    let result = start();
+    let args = get_cli_inputs();
+    let result = start(&args);
 
     if let Err(err) = result {
         eprintln!("{}", err);
--- src/wstring.rs
+++ src/wstring.rs
@@ -1,43 +1,45 @@
+#![allow(dead_code)]
+
 use ::std::fmt;
 
 #[derive(Default, Debug)]
 pub struct WideString {
-    inner: Vec<u16>,
+    pub bytes: Vec<u16>,
 }
 
 impl WideString {
     pub fn ptr(&self) -> *const u16 {
-        self.inner.as_ptr()
+        self.bytes.as_ptr()
     }
 
     pub fn mut_ptr(&mut self) -> *mut u16 {
-        self.inner.as_mut_ptr()
+        self.bytes.as_mut_ptr()
     }
 
     pub fn with_size(size: usize) -> Self {
         let mut vec = Vec::new();
         vec.resize(size, 0);
 
-        Self { inner: vec }
+        Self { bytes: vec }
     }
 
     pub fn from_str_with_size(text: &str, size: usize) -> Self {
         let mut vec = get_wide_string(text);
         vec.resize(size, 0);
 
-        Self { inner: vec }
+        Self { bytes: vec }
     }
 
     pub fn from_str(text: &str) -> Self {
         Self {
-            inner: get_wide_string(text),
+            bytes: get_wide_string(text),
         }
     }
 }
 
 impl fmt::Display for WideString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let string = String::from_utf16_lossy(&self.inner);
+        let string = String::from_utf16_lossy(&self.bytes);
         write!(f, "{}", string)
     }
 }
